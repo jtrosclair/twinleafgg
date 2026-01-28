d1b7ad5440e2e76010be38b905e50e1fa7bc8f5b
Jerry Trosclair <jtrosclair@live.com>
2026-01-27 20:20:47 -0600

    Update create_cards.py

--- create_cards.py
+++ create_cards.py
@@ -152,7 +152,7 @@ def implement_card_with_agent(card_data):
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            cwd="/srv/twinleafgg"
+            cwd="/home/ec2-user/twinleaf/twinleafgg"
         )
 
         # Send the prompt via stdin and get the response
