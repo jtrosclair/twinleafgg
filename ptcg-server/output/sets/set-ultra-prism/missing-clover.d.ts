ec63d61312d4da8a72b6f00f5c16f3cd03906c88
Jerry Trosclair <jtrosclair@live.com>
2026-05-06 14:16:37 -0500

    a

--- ptcg-server/output/sets/set-ultra-prism/missing-clover.d.ts
+++ ptcg-server/output/sets/set-ultra-prism/missing-clover.d.ts
@@ -1,12 +1,13 @@
-import { TrainerCard } from '../../game/store/card/trainer-card';
 import { TrainerType } from '../../game/store/card/card-types';
-import { StoreLike, State } from '../../game';
+import { TrainerCard } from '../../game/store/card/trainer-card';
 import { Effect } from '../../game/store/effects/effect';
+import { State } from '../../game/store/state/state';
+import { StoreLike } from '../../game/store/store-like';
 export declare class MissingClover extends TrainerCard {
     trainerType: TrainerType;
     set: string;
-    setNumber: string;
     cardImage: string;
+    setNumber: string;
     name: string;
     fullName: string;
     text: string;
